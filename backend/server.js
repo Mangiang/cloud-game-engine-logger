eebdef6de2e1c897e2d54a592baee33129acfca2
Mangiang <mangiang@orange.fr>
2020-07-24 18:24:39 -0400

    Display logs in the window

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@
 const express = require('express')
 Object.assign(global, { WebSocket: require('ws') });
 var StompJs = require('@stomp/stompjs');
+const { default: e } = require('express');
+const { json } = require('express');
 
 // Constants
 const WS_PORT = 8081
@@ -18,49 +20,54 @@ app.listen(HTTP_PORT, HOST)
 console.log(`HTTP Running on http://${HOST}:${HTTP_PORT}`)
 
 
-// Stomp cliend for RabbitMQ
-const client = new StompJs.Client({
-    brokerURL: "ws://rabbitmq.default.svc.cluster.local:15674/ws",
-    connectHeaders: {
-        login: "user",
-        passcode: "user"
-    },
-    debug: function(str) {
-        console.log(str);
-    },
-    reconnectDelay: 2000,
-    heartbeatIncoming: 1000,
-    heartbeatOutgoing: 1000
-});
-
-client.onConnect = function(frame) {
-    // Do something, all subscribes must be done is this callback
-    // This is needed because this will be executed after a (re)connect
-    console.log('Stomp Connected')
-    const subscription = client.subscribe('/queue/logs/info', function(
-        message,
-    ) {
-        console.log(message)
-    })
-};
-
-client.onStompError = function(frame) {
-    // Will be invoked in case of error encountered at Broker
-    // Bad login/passcode typically will cause an error
-    // Complaint brokers will set `message` header with a brief message. Body may contain details.
-    // Compliant brokers will terminate the connection after any error
-    console.log('Broker reported error: ' + frame.headers['message']);
-    console.log('Additional details: ' + frame.body);
-};
-client.activate();
-
-
 // Websocket for the frontend
 const wss = new WebSocket.Server({ host: HOST, port: WS_PORT });
+let ws_save = undefined
 wss.on('connection', function connection(ws) {
+    // Stomp cliend for RabbitMQ
+    const client = new StompJs.Client({
+        brokerURL: "ws://rabbitmq.default.svc.cluster.local:15674/ws",
+        connectHeaders: {
+            login: "user",
+            passcode: "user"
+        },
+        debug: function(str) {
+            console.log(str);
+        },
+        reconnectDelay: 2000,
+        heartbeatIncoming: 10000,
+        heartbeatOutgoing: 10000
+    });
+
+
+    client.onStompError = function(frame) {
+        // Will be invoked in case of error encountered at Broker
+        // Bad login/passcode typically will cause an error
+        // Complaint brokers will set `message` header with a brief message. Body may contain details.
+        // Compliant brokers will terminate the connection after any error
+        console.log('Broker reported error: ' + frame.headers['message']);
+        console.log('Additional details: ' + frame.body);
+    };
+    let subscription = null
+    client.onConnect = function(frame) {
+        // Do something, all subscribes must be done is this callback
+        // This is needed because this will be executed after a (re)connect
+        console.log('Stomp Connected')
+        subscription = client.subscribe('/queue/logs_info', function(
+            message,
+        ) {
+            console.log(message)
+            ws.send(JSON.stringify({ type: message.destination, value: message.body }));
+            console.log("message sent to the client")
+        })
+    };
+    client.activate();
+
     ws.on('message', function incoming(message) {
         console.log('received: %s', message);
     });
+
+    ws.on('close', (reasonCode, description) => { subscription.unsubscribe(); });
 
     // ws.send('something');
     console.log(`WS Running on ws://${HOST}:${WS_PORT}`)
--- frontend/src/components/Logger.tsx
+++ frontend/src/components/Logger.tsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { w3cwebsocket as W3CWebSocket, IMessageEvent } from 'websocket'
+import { useState } from 'react'
+
+// import { Client, Message, Frame } from '@stomp/stompjs'
+
+// export function connectRabbit() {}
 
 const Logger = () => {
+  const [ messages, setMessages ] = useState<string[]>([])
+  const [ client, setClient ] = useState<W3CWebSocket>()
+
+  useEffect(() => {
+    let myClient = new W3CWebSocket('ws://localhost/plugins/logger/ws')
+    myClient.onopen = () => {
+      console.log('WebSocket Client Connected')
+    }
+    myClient.onmessage = (message: IMessageEvent) => {
+      console.log(message)
+      const data = JSON.parse(message.data.toString());
+      setMessages(() => messages.concat([data.value]))
+    }
+    setClient(myClient)
+  }, [])
+
+  
+  const msgs = []
+  for (const [index, value] of messages.entries()) {
+    msgs.push(<li key={index}>{value}</li>)
+  }
+  
   return (
-    <div>
-      Logging
+    <div className="Logger">
+      {msgs}
     </div>
   )
 }
